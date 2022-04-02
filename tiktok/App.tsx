e3fcb5c79d1026983e59ba085d823ef1e91ab8c8
MartaGreen <iguana13131@gmail.com>
2022-04-02 17:00:30 +0400

    init: start tiktok-demo project

---
+++ tiktok/App.tsx
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Test text</Text>
+    </View>
+  );
+};
+
+export default App;
